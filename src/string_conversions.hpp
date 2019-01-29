8d16af1418722060cc735b5782e0470810dfd661
Ilya Seleznev <itsuart@gmail.com>
2019-01-29 13:20:12 +0700

    to_wide now accepts pointers to anything of size 1.

--- src/string_conversions.hpp
+++ src/string_conversions.hpp
@@ -2,6 +2,7 @@
 #define HELPERS_STRING_CONVERSIONS_HPP
 
 #include <memory>
+#include <type_traits>
 
 namespace helpers {
     // Converts null-terminated wide (utf16) string into null-terminated utf8-encoded string
@@ -11,6 +12,14 @@ namespace helpers {
     // Converts null-terminated utf8-encoded string into null-terminated wide string (utf16)
     // In case of error, result constains nullptr. Call GetLastError() to get the error code.
     std::unique_ptr<wchar_t[]> to_wide(const char* input);
+
+    // Converts null-terminated utf8-encoded string into null-terminated wide string (utf16)
+    // In case of error, result constains nullptr. Call GetLastError() to get the error code.
+    template<typename T, class = std::enable_if<sizeof(T) == 1>::type>
+    std::unique_ptr<wchar_t[]> to_wide(const T* input) {
+        return to_wide(reinterpret_cast<const char*>(input));
+    }
+
 }
 
 #endif
