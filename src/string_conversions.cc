5f1df46fe4859e18ac26ff89253ed6ac6e8ddd9e
Ilya Seleznev <itsuart@gmail.com>
2019-03-05 20:42:39 +0700

    add to_wstring to string_conversions fix an errwarning from VS2017 in template

--- src/string_conversions.cc
+++ src/string_conversions.cc
@@ -22,14 +22,31 @@ namespace helpers {
     std::unique_ptr<wchar_t[]> to_wide(const char* input) {
         std::unique_ptr<wchar_t[]> empty;
 
-        const std::size_t requiredSize = MultiByteToWideChar(CP_UTF8, 0, input, -1, nullptr, 0);
+        const std::size_t requiredSize = ::MultiByteToWideChar(CP_UTF8, 0, input, -1, nullptr, 0);
         if (!requiredSize) return empty;
 
         std::unique_ptr<wchar_t[]> result(new wchar_t[requiredSize]);
 
-        const std::size_t charsProcessed =
-            MultiByteToWideChar(CP_UTF8, 0, input, -1, result.get(), requiredSize);
+        const std::size_t charsProcessed = ::MultiByteToWideChar(CP_UTF8, 0, input, -1, result.get(), requiredSize);
         if (!charsProcessed) return empty;
+
+        return result;
+    }
+
+    std::wstring to_wstring(const char* input) {
+        std::wstring result;
+
+        const std::size_t requiredSize = ::MultiByteToWideChar(CP_UTF8, 0, input, -1, nullptr, 0);
+        if (!requiredSize) {
+            return result;
+        }
+
+        result.resize(requiredSize);
+
+        const std::size_t charsProcessed = ::MultiByteToWideChar(CP_UTF8, 0, input, -1, &result[0], requiredSize);
+        if (!charsProcessed) {
+            result.resize(0); // error
+        }
 
         return result;
     }
--- src/string_conversions.h
+++ src/string_conversions.h
@@ -2,24 +2,35 @@
 #define HELPERS_STRING_CONVERSIONS_H
 
 #include <memory>
+#include <string>
 #include <type_traits>
 
 namespace helpers {
     // Converts null-terminated wide (utf16) string into null-terminated utf8-encoded string
-    // In case of error, result constains nullptr. Call GetLastError() to get the error code.
+    // In case of error, result contains nullptr. Call GetLastError() to get the error code.
     std::unique_ptr<char[]> to_utf8(const wchar_t* input);
 
     // Converts null-terminated utf8-encoded string into null-terminated wide string (utf16)
-    // In case of error, result constains nullptr. Call GetLastError() to get the error code.
+    // In case of error, result contains nullptr. Call GetLastError() to get the error code.
     std::unique_ptr<wchar_t[]> to_wide(const char* input);
 
     // Converts null-terminated utf8-encoded string into null-terminated wide string (utf16)
     // In case of error, result constains nullptr. Call GetLastError() to get the error code.
-    template<typename T, class = std::enable_if<sizeof(T) == 1>::type>
+    template<typename T, class = typename std::enable_if<sizeof(T) == 1>::type>
     std::unique_ptr<wchar_t[]> to_wide(const T* input) {
         return to_wide(reinterpret_cast<const char*>(input));
     }
 
+    // Converts null-terminated utf8-encoded string into null-terminated wide string (utf16)
+    // In case of error, result is an empty string. Call GetLastError() to get the error code.
+    std::wstring to_wstring(const char* input);
+
+    // Converts null-terminated utf8-encoded string into null-terminated wide string (utf16)
+    // In case of error, result is an empty string. Call GetLastError() to get the error code.
+    template<typename T, class = typename std::enable_if<sizeof(T) == 1>::type>
+    std::wstring to_wstring(const T* input) {
+        return to_wstring(reinterpret_cast<const char*>(input));
+    }
 }
 
 #endif
